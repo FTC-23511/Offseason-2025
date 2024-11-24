ccadba0ef13b0f6a9456b80ad681a74516508fab
Saket T <saket4toshniwal@gmail.com>
2024-11-23 22:29:59 -0800

    burrito is not yummy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Auto/Burrito.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Auto/Burrito.java
@@ -9,15 +9,19 @@ import com.arcrobotics.ftclib.command.RunCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.hardware.Robot;
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.pedroPathing.localization.PoseUpdater;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathBuilder;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.DashboardPoseTracker;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.Drawing;
 import org.firstinspires.ftc.teamcode.subsystem.Deposit;
 import org.firstinspires.ftc.teamcode.subsystem.commands.FollowPathCommand;
 import org.firstinspires.ftc.teamcode.subsystem.commands.setDeposit;
@@ -30,9 +34,9 @@ public class Burrito extends CommandOpMode {
 
     private PathChain path;
 
+    private DashboardPoseTracker dashboardPoseTracker;
+    private Telemetry telemetry;
     public void generatePath() {
-        PathBuilder builder = robot.follower.pathBuilder();
-
         // If you want to edit the pathing copy and update the json code/.pp file found in the Recipes package into https://pedro-path-generator.vercel.app/
         // Then paste the following code https://pedro-path-generator.vercel.app/ spits out at you (excluding the top part with the class and constructor headers)
         // Make sure to update the Recipes package so others can update the pathing as well
@@ -49,74 +53,75 @@ public class Burrito extends CommandOpMode {
                                 new Point(40.000, 78.000, Point.CARTESIAN)
                         )
                 )
-                .setTangentHeadingInterpolation()
-                .addPath(
-                        // Line 2
-                        new BezierCurve(
-                                new Point(40.000, 78.000, Point.CARTESIAN),
-                                new Point(22.000, 78.000, Point.CARTESIAN),
-                                new Point(42.426, 121.539, Point.CARTESIAN),
-                                new Point(22.461, 123.286, Point.CARTESIAN)
-                        )
-                )
-                .setTangentHeadingInterpolation()
-                .setReversed(true)
-                .addPath(
-                        // Line 3
-                        new BezierCurve(
-                                new Point(22.461, 123.286, Point.CARTESIAN),
-                                new Point(15.972, 123.785, Point.CARTESIAN),
-                                new Point(12.977, 128.776, Point.CARTESIAN)
-                        )
-                )
-                .setTangentHeadingInterpolation()
-                .setReversed(true)
-                .addPath(
-                        // Line 4
-                        new BezierLine(
-                                new Point(12.977, 128.776, Point.CARTESIAN),
-                                new Point(23.958, 130.773, Point.CARTESIAN)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(-57), Math.toRadians(0))
-                .addPath(
-                        // Line 5
-                        new BezierLine(
-                                new Point(23.958, 130.773, Point.CARTESIAN),
-                                new Point(14.974, 130.024, Point.CARTESIAN)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(-45))
-                .setReversed(true)
-                .addPath(
-                        // Line 6
-                        new BezierLine(
-                                new Point(14.974, 130.024, Point.CARTESIAN),
-                                new Point(23.709, 132.520, Point.CARTESIAN)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(-45), Math.toRadians(25))
-                .addPath(
-                        // Line 7
-                        new BezierLine(
-                                new Point(23.709, 132.520, Point.CARTESIAN),
-                                new Point(14.974, 128.776, Point.CARTESIAN)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(25), Math.toRadians(-45))
-                .addPath(
-                        // Line 8
-                        new BezierCurve(
-                                new Point(14.974, 128.776, Point.CARTESIAN),
-                                new Point(63.706, 117.899, Point.CARTESIAN),
-                                new Point(62.157, 94.894, Point.CARTESIAN)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(-45), Math.toRadians(90)).build();
+                .setTangentHeadingInterpolation().build();
+//                .addPath(
+//                        // Line 2
+//                        new BezierCurve(
+//                                new Point(40.000, 78.000, Point.CARTESIAN),
+//                                new Point(22.000, 78.000, Point.CARTESIAN),
+//                                new Point(42.426, 121.539, Point.CARTESIAN),
+//                                new Point(22.461, 123.286, Point.CARTESIAN)
+//                        )
+//                )
+//                .setTangentHeadingInterpolation()
+//                .setReversed(true)
+//                .addPath(
+//                        // Line 3
+//                        new BezierCurve(
+//                                new Point(22.461, 123.286, Point.CARTESIAN),
+//                                new Point(15.972, 123.785, Point.CARTESIAN),
+//                                new Point(12.977, 128.776, Point.CARTESIAN)
+//                        )
+//                )
+//                .setTangentHeadingInterpolation()
+//                .setReversed(true)
+//                .addPath(
+//                        // Line 4
+//                        new BezierLine(
+//                                new Point(12.977, 128.776, Point.CARTESIAN),
+//                                new Point(23.958, 130.773, Point.CARTESIAN)
+//                        )
+//                )
+//                .setLinearHeadingInterpolation(Math.toRadians(-57), Math.toRadians(0))
+//                .addPath(
+//                        // Line 5
+//                        new BezierLine(
+//                                new Point(23.958, 130.773, Point.CARTESIAN),
+//                                new Point(14.974, 130.024, Point.CARTESIAN)
+//                        )
+//                )
+//                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(-45))
+//                .setReversed(true)
+//                .addPath(
+//                        // Line 6
+//                        new BezierLine(
+//                                new Point(14.974, 130.024, Point.CARTESIAN),
+//                                new Point(23.709, 132.520, Point.CARTESIAN)
+//                        )
+//                )
+//                .setLinearHeadingInterpolation(Math.toRadians(-45), Math.toRadians(25))
+//                .addPath(
+//                        // Line 7
+//                        new BezierLine(
+//                                new Point(23.709, 132.520, Point.CARTESIAN),
+//                                new Point(14.974, 128.776, Point.CARTESIAN)
+//                        )
+//                )
+//                .setLinearHeadingInterpolation(Math.toRadians(25), Math.toRadians(-45))
+//                .addPath(
+//                        // Line 8
+//                        new BezierCurve(
+//                                new Point(14.974, 128.776, Point.CARTESIAN),
+//                                new Point(63.706, 117.899, Point.CARTESIAN),
+//                                new Point(62.157, 94.894, Point.CARTESIAN)
+//                        )
+//                )
+//                .setLinearHeadingInterpolation(Math.toRadians(-45), Math.toRadians(90)).build();
     }
     @Override
     public void initialize() {
         opModeType = OpModeType.AUTO;
+        depositInit = DepositInit.SPECIMEN_SCORING;
 
         // DO NOT REMOVE! Resetting FTCLib Command Scheduler
         super.reset();
@@ -135,20 +140,24 @@ public class Burrito extends CommandOpMode {
         schedule(
                 new RunCommand(() -> robot.follower.update()),
                 new SequentialCommandGroup(
-                        new FollowPathCommand(robot.follower, path),
-                        new setDeposit(robot.deposit, Deposit.DepositPivotState.SCORING, LOW_BUCKET_HEIGHT)
+                        new FollowPathCommand(robot.follower, path)
+//                        new setDeposit(robot.deposit, Deposit.DepositPivotState.SCORING, LOW_BUCKET_HEIGHT)
                 )
         );
+
+        dashboardPoseTracker = new DashboardPoseTracker(robot.follower.poseUpdater);
+        Drawing.drawRobot(robot.follower.poseUpdater.getPose(), "#4CAF50");
+        Drawing.sendPacket();
     }
 
     @Override
     public void run() {
         super.run();
 
-        telemetry.addData("target", robot.deposit.target);
-        telemetry.addData("depositPos", robot.liftEncoder.getPosition());
-        telemetry.addData("liftBottom Motor Power", robot.liftBottom.getPower());
-        telemetry.addData("liftTop Motor Power", robot.liftTop.getPower());
-        telemetry.update();
+        // Pathing telemetry
+        dashboardPoseTracker.update();
+        Drawing.drawPoseHistory(dashboardPoseTracker, "#4CAF50");
+        Drawing.drawRobot(robot.follower.poseUpdater.getPose(), "#4CAF50");
+        Drawing.sendPacket();
     }
 }
