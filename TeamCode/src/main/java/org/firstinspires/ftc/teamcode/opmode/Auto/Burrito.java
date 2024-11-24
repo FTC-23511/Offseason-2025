ecb4fda1a7230e66e411809177ba70c758059537
Saket T <saket4toshniwal@gmail.com>
2024-11-24 09:08:56 -0800

    burrito is still not tasty

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Auto/Burrito.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Auto/Burrito.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode.opmode.Auto;
 
+import java.util.ArrayList;
+
 import static org.firstinspires.ftc.teamcode.hardware.Globals.*;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.RunCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -23,8 +27,12 @@ import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
 import org.firstinspires.ftc.teamcode.pedroPathing.util.DashboardPoseTracker;
 import org.firstinspires.ftc.teamcode.pedroPathing.util.Drawing;
 import org.firstinspires.ftc.teamcode.subsystem.Deposit;
+import org.firstinspires.ftc.teamcode.subsystem.Intake;
 import org.firstinspires.ftc.teamcode.subsystem.commands.FollowPathCommand;
+import org.firstinspires.ftc.teamcode.subsystem.commands.attachSpecimen;
+import org.firstinspires.ftc.teamcode.subsystem.commands.realTransfer;
 import org.firstinspires.ftc.teamcode.subsystem.commands.setDeposit;
+import org.firstinspires.ftc.teamcode.subsystem.commands.setExtendo;
 
 @Config
 @Autonomous(name = "Burrito (1spec+3sample)", group = "Chipotle Menu", preselectTeleOp = "FullTeleOp")
@@ -32,7 +40,7 @@ import org.firstinspires.ftc.teamcode.subsystem.commands.setDeposit;
 public class Burrito extends CommandOpMode {
     private final Robot robot = Robot.getInstance();
 
-    private PathChain path;
+    private ArrayList<PathChain> paths = new ArrayList<>();
 
     private DashboardPoseTracker dashboardPoseTracker;
     private Telemetry telemetry;
@@ -40,83 +48,112 @@ public class Burrito extends CommandOpMode {
         // If you want to edit the pathing copy and update the json code/.pp file found in the Recipes package into https://pedro-path-generator.vercel.app/
         // Then paste the following code https://pedro-path-generator.vercel.app/ spits out at you (excluding the top part with the class and constructor headers)
         // Make sure to update the Recipes package so others can update the pathing as well
-        // NOTE: .setTangentialHeadingInterpolation() doesn't exist its .setTangentHeadingInterpolation() so just fix that whenever you paste
+        // NOTE: .setTangentHeadingInterpolation() doesn't exist its .setTangentHeadingInterpolation() so just fix that whenever you paste
 
         // Starting Pose (update this as well):
-        robot.follower.setStartingPose(new Pose(6.125, 78, 0));
+        robot.follower.setStartingPose(new Pose(6.125, 78, 180));
 
-        path = robot.follower.pathBuilder()
+        paths.add(
+                // Drive to first specimen scoring
+                robot.follower.pathBuilder()
                 .addPath(
                         // Line 1
                         new BezierLine(
                                 new Point(6.125, 78.000, Point.CARTESIAN),
-                                new Point(40.000, 78.000, Point.CARTESIAN)
+                                new Point(38.000, 78.000, Point.CARTESIAN)
                         )
                 )
-                .setTangentHeadingInterpolation().build();
-//                .addPath(
-//                        // Line 2
-//                        new BezierCurve(
-//                                new Point(40.000, 78.000, Point.CARTESIAN),
-//                                new Point(22.000, 78.000, Point.CARTESIAN),
-//                                new Point(42.426, 121.539, Point.CARTESIAN),
-//                                new Point(22.461, 123.286, Point.CARTESIAN)
-//                        )
-//                )
-//                .setTangentHeadingInterpolation()
-//                .setReversed(true)
-//                .addPath(
-//                        // Line 3
-//                        new BezierCurve(
-//                                new Point(22.461, 123.286, Point.CARTESIAN),
-//                                new Point(15.972, 123.785, Point.CARTESIAN),
-//                                new Point(12.977, 128.776, Point.CARTESIAN)
-//                        )
-//                )
-//                .setTangentHeadingInterpolation()
-//                .setReversed(true)
-//                .addPath(
-//                        // Line 4
-//                        new BezierLine(
-//                                new Point(12.977, 128.776, Point.CARTESIAN),
-//                                new Point(23.958, 130.773, Point.CARTESIAN)
-//                        )
-//                )
-//                .setLinearHeadingInterpolation(Math.toRadians(-57), Math.toRadians(0))
-//                .addPath(
-//                        // Line 5
-//                        new BezierLine(
-//                                new Point(23.958, 130.773, Point.CARTESIAN),
-//                                new Point(14.974, 130.024, Point.CARTESIAN)
-//                        )
-//                )
-//                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(-45))
-//                .setReversed(true)
-//                .addPath(
-//                        // Line 6
-//                        new BezierLine(
-//                                new Point(14.974, 130.024, Point.CARTESIAN),
-//                                new Point(23.709, 132.520, Point.CARTESIAN)
-//                        )
-//                )
-//                .setLinearHeadingInterpolation(Math.toRadians(-45), Math.toRadians(25))
-//                .addPath(
-//                        // Line 7
-//                        new BezierLine(
-//                                new Point(23.709, 132.520, Point.CARTESIAN),
-//                                new Point(14.974, 128.776, Point.CARTESIAN)
-//                        )
-//                )
-//                .setLinearHeadingInterpolation(Math.toRadians(25), Math.toRadians(-45))
-//                .addPath(
-//                        // Line 8
-//                        new BezierCurve(
-//                                new Point(14.974, 128.776, Point.CARTESIAN),
-//                                new Point(63.706, 117.899, Point.CARTESIAN),
-//                                new Point(62.157, 94.894, Point.CARTESIAN)
-//                        )
-//                )
-//                .setLinearHeadingInterpolation(Math.toRadians(-45), Math.toRadians(90)).build();
+                .setTangentHeadingInterpolation()
+                .setReversed(true).build());
+
+        paths.add(
+                // Drive to first sample intake
+                robot.follower.pathBuilder()
+                .addPath(
+                        // Line 2
+                        new BezierCurve(
+                                new Point(38.000, 78.000, Point.CARTESIAN),
+                                new Point(22.000, 78.000, Point.CARTESIAN),
+                                new Point(42.426, 121.539, Point.CARTESIAN),
+                                new Point(22.461, 123.286, Point.CARTESIAN)
+                        )
+                )
+                .setTangentHeadingInterpolation().build());
+
+        paths.add(
+                // Drive to first sample scoring
+                robot.follower.pathBuilder()
+                .addPath(
+                        // Line 3
+                        new BezierCurve(
+                                new Point(22.461, 123.286, Point.CARTESIAN),
+                                new Point(15.972, 123.785, Point.CARTESIAN),
+                                new Point(12.977, 128.776, Point.CARTESIAN)
+                        )
+                )
+                .setTangentHeadingInterpolation().build());
+
+        paths.add(
+                // Drive to second sample intake
+                robot.follower.pathBuilder()
+                .addPath(
+                        // Line 4
+                        new BezierLine(
+                                new Point(12.977, 128.776, Point.CARTESIAN),
+                                new Point(23.958, 130.773, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(123), Math.toRadians(180)).build());
+
+        paths.add(
+                // Drive to second sample scoring
+                robot.follower.pathBuilder()
+                .addPath(
+                        // Line 5
+                        new BezierLine(
+                                new Point(23.958, 130.773, Point.CARTESIAN),
+                                new Point(14.974, 130.024, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(135))
+                .setReversed(true).build());
+
+        paths.add(
+                // Drive to third sample intake
+                robot.follower.pathBuilder()
+                .addPath(
+                        // Line 6
+                        new BezierLine(
+                                new Point(14.974, 130.024, Point.CARTESIAN),
+                                new Point(23.709, 132.520, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(-155)).build());
+
+        paths.add(
+                // Drive to third sample scoring
+                robot.follower.pathBuilder()
+                .addPath(
+                        // Line 7
+                        new BezierLine(
+                                new Point(23.709, 132.520, Point.CARTESIAN),
+                                new Point(14.974, 128.776, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(-155), Math.toRadians(135)).build());
+
+        paths.add(
+                // Park/ascent level 1
+                robot.follower.pathBuilder()
+                .addPath(
+                        // Line 8
+                        new BezierCurve(
+                                new Point(14.974, 128.776, Point.CARTESIAN),
+                                new Point(63.706, 117.899, Point.CARTESIAN),
+                                new Point(62.157, 94.894, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(-90)).build());
     }
     @Override
     public void initialize() {
@@ -135,13 +172,31 @@ public class Burrito extends CommandOpMode {
 
         robot.initHasMovement();
 
+        robot.follower.setMaxPower(0.5);
+
         generatePath();
 
         schedule(
                 new RunCommand(() -> robot.follower.update()),
                 new SequentialCommandGroup(
-                        new FollowPathCommand(robot.follower, path)
-//                        new setDeposit(robot.deposit, Deposit.DepositPivotState.SCORING, LOW_BUCKET_HEIGHT)
+                        new ParallelCommandGroup(
+//                                new setDeposit(robot.deposit, Deposit.DepositPivotState.SPECIMEN_SCORING, HIGH_SPECIMEN_HEIGHT),
+                                new FollowPathCommand(robot.follower, paths.get(1))),
+//                        new attachSpecimen(robot.deposit),
+                        new ParallelCommandGroup(
+//                                new setDeposit(robot.deposit, Deposit.DepositPivotState.MIDDLE_HOLD, 0),
+                                new FollowPathCommand(robot.follower, paths.get(2))),
+//                        new ParallelCommandGroup(
+//                                new setExtendo(robot.deposit, robot.intake, 500),
+//                                new InstantCommand(() -> {
+//                                    robot.intake.setPivot(Intake.IntakePivotState.INTAKE);
+//                                    Intake.sampleColorTarget = Intake.SampleColorTarget.ANY_COLOR;
+//                                    robot.intake.setActiveIntake(Intake.IntakeMotorState.FORWARD);
+//                                }))
+                        new FollowPathCommand(robot.follower, paths.get(3)),
+                        new FollowPathCommand(robot.follower, paths.get(4)),
+                        new FollowPathCommand(robot.follower, paths.get(5)),
+                        new FollowPathCommand(robot.follower, paths.get(6))
                 )
         );
 
