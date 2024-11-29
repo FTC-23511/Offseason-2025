dcba7a037d8ffd210ebd1394acece9af43dfdf4e
Saket T <saket4toshniwal@gmail.com>
2024-11-28 16:53:26 -0800

    please help if this works
    :skull:

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/commands/UninterruptibleCommand.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.commandbase.commands;
+
+import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+
+public class UninterruptibleCommand extends CommandBase {
+    private final Command command;
+
+    public UninterruptibleCommand(Command command) {
+        this.command = command;
+    }
+
+    @Override
+    public void initialize() {
+        command.schedule(false);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return !CommandScheduler.getInstance().isScheduled(command);
+    }
+}
